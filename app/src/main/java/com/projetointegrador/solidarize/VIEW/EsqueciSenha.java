3ebaa591730e968013cca2221da77f04ccc204d7
luana-q-g <queiroz.l@aluno.ifsp.edu.br>
2019-08-10 20:30:04 -0300

    Tela Esqueci Senha

---
+++ app/src/main/java/com/projetointegrador/solidarize/VIEW/EsqueciSenha.java
@@ -0,0 +1,17 @@
+package com.projetointegrador.solidarize.VIEW;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.projetointegrador.solidarize.R;
+
+public class EsqueciSenha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_esqueci_senha);
+
+
+    }
+}
