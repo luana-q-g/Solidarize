7e34c19afc90551b1a1d25820fb151229dc87a10
luana-q-g <queiroz.l@aluno.ifsp.edu.br>
2019-08-22 16:12:41 -0300

    Acertando errinhos do NavDrawMenu.java

--- app/src/main/java/com/projetointegrador/solidarize/VIEW/NavDrawMenu.java
+++ app/src/main/java/com/projetointegrador/solidarize/VIEW/NavDrawMenu.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.projetointegrador.solidarize.R;
+
 public class NavDrawMenu extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -22,15 +24,6 @@ public class NavDrawMenu extends AppCompatActivity
         setContentView(R.layout.activity_nav_draw_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -52,46 +45,48 @@ public class NavDrawMenu extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.nav_draw_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        switch (id) {
+            case R.id.nav_perfil:
 
-        } else if (id == R.id.nav_slideshow) {
+                break;
 
-        } else if (id == R.id.nav_manage) {
+            case R.id.nav_acoes_eventos:
 
-        } else if (id == R.id.nav_share) {
+                break;
 
-        } else if (id == R.id.nav_send) {
+            case R.id.nav_acoes_pedidos_doacao:
 
+                break;
+
+            case R.id.nav_eventos:
+
+                break;
+
+            case R.id.nav_inst_todas:
+
+                break;
+
+            case R.id.nav_inst_pedidos:
+
+                break;
+
+            case R.id.nav_config:
+
+                break;
+
+            case R.id.nav_sobre_app:
+
+                break;
+
+            case R.id.nav_sair:
+
+                break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
