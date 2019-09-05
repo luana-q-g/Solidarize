b47246356fc6f7b11267256b8fc23d97e54dffa8
luana-q-g <queiroz.l@aluno.ifsp.edu.br>
2019-09-05 11:25:06 -0300

    Transferência para AndroidX!!

--- app/src/main/java/com/projetointegrador/solidarize/VIEW/CadastroInstituicao.java
+++ app/src/main/java/com/projetointegrador/solidarize/VIEW/CadastroInstituicao.java
@@ -1,6 +1,6 @@
 package com.projetointegrador.solidarize.VIEW;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.projetointegrador.solidarize.R;
