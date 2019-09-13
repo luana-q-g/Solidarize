873e705a9d7adbd36d9b3f27395a2f5b596b2104
luana-q-g <queiroz.l@aluno.ifsp.edu.br>
2019-09-13 12:35:40 -0300

    Transição de telas de cadastro de pessoa

--- app/src/main/java/com/projetointegrador/solidarize/VIEW/CadastroPessoa.java
+++ app/src/main/java/com/projetointegrador/solidarize/VIEW/CadastroPessoa.java
@@ -1,15 +1,80 @@
 package com.projetointegrador.solidarize.VIEW;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.projetointegrador.solidarize.BEAN.Pessoa;
+import com.projetointegrador.solidarize.BEAN.TelefoneUsuario;
 import com.projetointegrador.solidarize.R;
 
 public class CadastroPessoa extends AppCompatActivity {
+    private FrameLayout place_holder;
+
+    private String nome;
+    private String email;
+    private String cpf;
+    private String telefone;
+    private String dt_nasc;
+    private String cidade;
+    private String estado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_pessoa);
+
+        place_holder= findViewById(R.id.place_holder_info_cadastro_pessoa);
+
+        FragmentManager fm= getSupportFragmentManager();
+        FragmentTransaction ft= fm.beginTransaction();
+
+        CadastroPessoaDadosPessoaisFragment cadastro_dados_pessoais= new CadastroPessoaDadosPessoaisFragment();
+        ft.replace(R.id.place_holder_info_cadastro_pessoa, cadastro_dados_pessoais);
+        ft.commit();
+    }
+
+    public void setDadosPessoais(Pessoa pessoa, TelefoneUsuario tel){
+        this.nome= pessoa.getNome();
+        this.email= pessoa.getEmail();
+        this.cpf= pessoa.getCpf();
+        this.telefone= tel.getTelefone();
+        this.dt_nasc= pessoa.getData_nasc();
+    }
+
+    public void setEndereco(String c, String e){
+        this.estado= e;
+        this.cidade= c;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getDt_nasc() {
+        return dt_nasc;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getEstado() {
+        return estado;
     }
 }
--- app/src/main/java/com/projetointegrador/solidarize/VIEW/CadastroPessoaDadosPessoaisFragment.java
+++ app/src/main/java/com/projetointegrador/solidarize/VIEW/CadastroPessoaDadosPessoaisFragment.java
@@ -4,11 +4,27 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.projetointegrador.solidarize.BEAN.Pessoa;
+import com.projetointegrador.solidarize.BEAN.TelefoneUsuario;
+import com.projetointegrador.solidarize.R;
 
 public class CadastroPessoaDadosPessoaisFragment extends Fragment {
+    private EditText nome;
+    private EditText email;
+    private EditText cpf;
+    private EditText telefone;
+    private EditText dt_nasc;
+
+    private Button btn_continuar;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,6 +34,45 @@ public class CadastroPessoaDadosPessoaisFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view= inflater.inflate(R.layout.fragment_act_cadastro_pessoa_dados_pessoais, container, false);
+
+        nome= view.findViewById(R.id.txt_nome);
+        email= view.findViewById(R.id.txt_email);
+        cpf= view.findViewById(R.id.txt_cpf);
+        telefone= view.findViewById(R.id.txt_telefone);
+        dt_nasc= view.findViewById(R.id.txt_dt_nasc);
+        btn_continuar= view.findViewById(R.id.btn_continuar);
+
+        btn_continuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*String n, e, c, t, d;
+                n= nome.getText().toString();
+                e= email.getText().toString();
+                c= cpf.getText().toString();
+                t= telefone.getText().toString();
+                d= dt_nasc.getText().toString();
+
+                Pessoa pessoa= new Pessoa();
+                pessoa.setNome(n);
+                pessoa.setEmail(e);
+                pessoa.setCpf(c);
+                pessoa.setData_nasc(d);
+
+                TelefoneUsuario tel= new TelefoneUsuario(e, t);
+
+                CadastroPessoa act= (CadastroPessoa) getActivity();
+                act.setDadosPessoais(pessoa, tel);*/
+
+                FragmentManager fm= getActivity().getSupportFragmentManager();
+                FragmentTransaction ft= fm.beginTransaction();
+
+                CadastroPessoaEnderecoFragment cadastro_endereco= new CadastroPessoaEnderecoFragment();
+                ft.replace(R.id.place_holder_info_cadastro_pessoa, cadastro_endereco);
+                ft.commit();
+            }
+        });
+
+        return view;
     }
 }
