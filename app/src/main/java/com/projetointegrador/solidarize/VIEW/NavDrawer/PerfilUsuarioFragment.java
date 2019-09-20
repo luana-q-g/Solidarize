4ce596ca70d48683be2e4de3bdfd31e84febbed6
bruna-g-p <bruna.g.peres@gmail.com>
2019-09-20 12:29:13 -0300

    Criação de layout do perfil de pessoa

--- app/src/main/java/com/projetointegrador/solidarize/VIEW/NavDrawer/PerfilUsuarioFragment.java
+++ app/src/main/java/com/projetointegrador/solidarize/VIEW/NavDrawer/PerfilUsuarioFragment.java
@@ -20,7 +20,7 @@ public class PerfilUsuarioFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.fragment_perfil_usuario, container, false);
+        View view= inflater.inflate(R.layout.fragment_perfil_pessoa, container, false);
 
         return view;
     }
