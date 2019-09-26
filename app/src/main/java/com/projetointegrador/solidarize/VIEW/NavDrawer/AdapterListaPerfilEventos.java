9e65a47d2f306bd6f7f476c79bc14ffd97fe02f0
luana-q-g <queiroz.l@aluno.ifsp.edu.br>
2019-09-26 10:35:36 -0300

    Criação adapter perfil_eventos_fragment

---
+++ app/src/main/java/com/projetointegrador/solidarize/VIEW/NavDrawer/AdapterListaPerfilEventos.java
@@ -0,0 +1,41 @@
+package com.projetointegrador.solidarize.VIEW.NavDrawer;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.projetointegrador.solidarize.BEAN.Evento;
+import com.projetointegrador.solidarize.R;
+
+import java.util.ArrayList;
+
+public class AdapterListaPerfilEventos extends ArrayAdapter<Evento> {
+    private Context context;
+    private ArrayList<Evento> eventos;
+
+    public AdapterListaPerfilEventos(Context c, ArrayList<Evento> us){
+        super(c, R.layout.item_edicao_evento, us);
+        this.context= c;
+        this.eventos= us;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        LayoutInflater li= LayoutInflater.from(parent.getContext());
+
+        View itemView= li.inflate(R.layout.item_edicao_evento, parent, false);
+
+        //ImageView img_topin= itemView.findViewById(R.id.img_topin);
+        TextView lbl_nome_evento= itemView.findViewById(R.id.lbl_nome_evento);
+
+        lbl_nome_evento.setText(eventos.get(position).getNome());
+
+        return itemView;
+    }
+
+}
--- app/src/main/java/com/projetointegrador/solidarize/VIEW/NavDrawer/TabLayoutPerfilEventosFragment.java
+++ app/src/main/java/com/projetointegrador/solidarize/VIEW/NavDrawer/TabLayoutPerfilEventosFragment.java
@@ -4,14 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.projetointegrador.solidarize.BEAN.Evento;
 import com.projetointegrador.solidarize.R;
+
+import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class TabLayoutPerfilEventosFragment extends Fragment {
+    private TextView lbl_existencia_eventos;
+    private ListView lista_eventos_criados;
+    private Button btn_criar_evento;
+
+    private ArrayList<Evento> eventos= new ArrayList<>();
+    private AdapterListaPerfilEventos adapter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,6 +34,11 @@ public class TabLayoutPerfilEventosFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_tab_layout_perfil_eventos, container, false);
+
+        adapter = new AdapterListaPerfilEventos(getActivity().getApplicationContext(), eventos);
+        lista_eventos_criados.setAdapter(adapter);
+
+        //dados.addValueEventListener(new EscutadorFirebase());
 
         return view;
     }
